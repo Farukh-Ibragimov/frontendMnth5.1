5b677c553dbce86e8c7a9cb4ce76bfc4c5f555cd
Farukh <farukh31082005@gmail.com>
2024-07-24 11:11:49 +0600

    HW

--- lesson5/src/App.js
+++ lesson5/src/App.js
@@ -3,6 +3,7 @@ import PostPage from "./pages/postPage/PostPage";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import PostsCardId from "./components/PostsCardId";
+import CreatePostPage from "./pages/createPostPage/CreatePostPage";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
           <Routes>
               <Route index element={<PostPage/>}/>
               <Route path='posts/:id' element={<PostsCardId/>}/>
+              <Route path='posts_register' element={<CreatePostPage/>}/>
           </Routes>
       </BrowserRouter>
   );
